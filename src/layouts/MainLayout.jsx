391d17ca33b4fd11cc073c4cc94701873f1bfe22
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-07 23:30:38 +0300

    Create MainLayout component with layout structure

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/common/Navbar';
+import Footer from '../components/common/Footer';
+import AuthModal from '../components/common/AuthModal';
+import ChatWidget from '../components/common/ChatWidget';
+
+function MainLayout() {
+  return (
+    <>
+      <Navbar />
+      <main id="main-content">
+        <Outlet />
+      </main>
+      <Footer />
+      <AuthModal />
+      <ChatWidget />
+      {/* Back to top, sticky CTA, resource modal are handled inside HomePage */}
+    </>
+  );
+}
+
+export default MainLayout;
