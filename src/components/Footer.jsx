652b323f14ab82ea6193505c639ba2cca9eac232
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-07 21:51:13 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ background: '#333', color: 'white', textAlign: 'center', padding: '1rem' }}>
+      <p>© 2024 Simple Four‑Page Site</p>
+    </footer>
+  )
+}
+
+export default Footer
