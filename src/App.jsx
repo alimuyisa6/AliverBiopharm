0c81e7a4f23f26bada9dc498df4f3da49dd86c6f
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-09 22:33:16 +0300

    Refactor import paths and add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { ThemeProvider } from './context/ThemeContext';
 import { LegalDataProvider } from './context/LegalDataContext';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import OLevelBiology from './pages/OLevelBiology';
+import OLevelBiology from './pages/notes/OLevelBiology';
+import ALevelBiology from './pages/notes/ALevelBiology';
+import PharmacyBiology from './pages/notes/PharmacyBiology';
 import TermsOfService from './pages/legal/TermsOfService';
 import PrivacyPolicy from './pages/legal/PrivacyPolicy';
-import NotePage from './pages/notes/NotePage'; 
+import NotePage from './pages/notes/NotePage';
 
 function App() {
   return (
@@ -19,9 +21,11 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/index.html" element={<Navigate to="/" replace />} />
               <Route path="/notes/olevel" element={<OLevelBiology />} />
+              <Route path="/notes/alevel" element={<ALevelBiology />} />
+              <Route path="/notes/pharmacy" element={<PharmacyBiology />} />
               <Route path="/terms" element={<TermsOfService />} />
               <Route path="/privacy" element={<PrivacyPolicy />} />
-              <Route path="/note/:id" element={<NotePage />} /> {/* <-- add this line */}
+              <Route path="/note/:id" element={<NotePage />} />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
           </Routes>
