7fd25b83277a494eace003c9d945c3da664706e8
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-09 19:46:53 +0300

    Add Terms of Service and Privacy Policy routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from './context/ThemeContext';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import OLevelBiology from './pages/OLevelBiology';
+import TermsOfService from './pages/legal/TermsOfService';
+import PrivacyPolicy from './pages/legal/PrivacyPolicy';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/index.html" element={<Navigate to="/" replace />} />
             <Route path="/notes/olevel" element={<OLevelBiology />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
