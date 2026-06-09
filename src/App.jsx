1bb8cbb6e22307e72ac8dd494b283d325737bd70
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-09 09:32:44 +0300

    Add Navigate route for index.html in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-  import { Routes, Route } from 'react-router-dom';
+ import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import MainLayout from './layouts/MainLayout';
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/index.html" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </AuthProvider>
