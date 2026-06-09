160a965c695fc9e5677198ad11bc9ef340ea270d
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-09 21:08:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
  import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { LegalDataProvider } from './context/LegalDataContext';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import OLevelBiology from './pages/OLevelBiology';
@@ -11,16 +12,18 @@ function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/index.html" element={<Navigate to="/" replace />} />
-            <Route path="/notes/olevel" element={<OLevelBiology />} />
-            <Route path="/terms" element={<TermsOfService />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Route>
-        </Routes>
+        <LegalDataProvider>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/index.html" element={<Navigate to="/" replace />} />
+              <Route path="/notes/olevel" element={<OLevelBiology />} />
+              <Route path="/terms" element={<TermsOfService />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Route>
+          </Routes>
+        </LegalDataProvider>
       </AuthProvider>
     </ThemeProvider>
   );
