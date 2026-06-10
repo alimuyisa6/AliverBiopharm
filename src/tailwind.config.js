1b93eb7702834f28b6009ab6e0f479012bb2157a
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-11 00:56:05 +0300

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif'],
+        'mono': ['DM Mono', 'monospace'],
+      }
+    },
+  },
+  plugins: [],
+}
