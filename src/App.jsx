4f1fae458d89ed11d09d3984b4c260c7efc7d4cf
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 01:26:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
- function App() {
-  return React.createElement('h1', { style: { color: 'red' } }, 'APP WORKS');
+ import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    React.createElement(BrowserRouter, null,
+      React.createElement(AuthProvider, null,
+        React.createElement(Routes, null,
+          React.createElement(Route, { path: "/login", element: React.createElement(Login, null) }),
+          React.createElement(Route, { path: "/register", element: React.createElement(Register, null) }),
+          React.createElement(Route, { path: "*", element: React.createElement(Home, null) })
+        )
+      )
+    )
+  );
 }
 
 export default App;
