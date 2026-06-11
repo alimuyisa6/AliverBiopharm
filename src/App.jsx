c1f596aa5c85ca660dc66f79b1abba12bb79b8df
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-11 15:55:28 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
- // App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import NotePage from './pages/NotePage';
 
 function App() {
   return (
@@ -13,11 +11,6 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/note/:id" element={
-            <ProtectedRoute>
-              <NotePage />
-            </ProtectedRoute>
-          } />
           <Route path="*" element={
             <ProtectedRoute>
               <Home />
