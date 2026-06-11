d3ce9a54c917525672992c44a46346230ce1b905
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 01:18:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
- import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    React.createElement(BrowserRouter, null,
-      React.createElement(Routes, null,
-        React.createElement(Route, { path: "*", element: React.createElement('h1', { style: { color: 'red' } }, 'APP WORKS') })
-      )
-    )
-  );
+ function App() {
+  return React.createElement('h1', { style: { color: 'red' } }, 'APP WORKS');
 }
 
 export default App;
