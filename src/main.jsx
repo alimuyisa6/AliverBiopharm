1da028e8653d61ff4fc596f1a3fce76e7a168d21
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 01:22:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-  import React from 'react'
+ import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+function App() {
+  return React.createElement('h1', { style: { color: 'red' } }, 'APP WORKS');
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(App)
 )
