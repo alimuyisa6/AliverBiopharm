7073470145de5943f0e542b6cfd628ff7fd0c697
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-11 15:37:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
- import { BrowserRouter, Routes, Route } from 'react-router-dom';
+ // App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
