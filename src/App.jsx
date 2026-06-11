2441dd8bbe07857a0936b3ad1dc339bd4493849e
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-11 16:14:33 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    React.createElement(BrowserRouter, null,
+      React.createElement(AuthProvider, null,
+        React.createElement(Routes, null,
+          React.createElement(Route, { path: "/login", element: React.createElement(Login, null) }),
+          React.createElement(Route, { path: "/register", element: React.createElement(Register, null) }),
+          React.createElement(Route, { path: "*", element: React.createElement(ProtectedRoute, null, React.createElement(Home, null)) })
+        )
+      )
+    )
+  );
+}
+
+export default App;
