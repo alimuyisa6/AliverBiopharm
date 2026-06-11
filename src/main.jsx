2c36a771091305618082eaa6f32e94bd608fc967
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 01:25:47 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
  import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-function App() {
-  return React.createElement('h1', { style: { color: 'red' } }, 'APP WORKS');
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   React.createElement(App)
