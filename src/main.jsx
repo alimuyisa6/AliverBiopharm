92d5c2b51fa5912a463866d7f507a9d7849297fe
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 01:51:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-window.onerror = function(msg, src, line) {
-  document.body.innerHTML = '<div style="color:red;padding:20px;font-size:18px">' + msg + ' (line ' + line + ')</div>';
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(App)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
