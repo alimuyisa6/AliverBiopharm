8208160a4c8bd56d1f485a473808b8bbc438cbc6
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-12 20:35:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
  import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Quiz from './pages/Quiz';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         React.createElement(Routes, null,
           React.createElement(Route, { path: "/login", element: React.createElement(Login, null) }),
           React.createElement(Route, { path: "/register", element: React.createElement(Register, null) }),
-          React.createElement(Route, { path: "*", element: React.createElement(Home, null) })
+          React.createElement(Route, { path: "/quiz", element: React.createElement(ProtectedRoute, null, React.createElement(Quiz, null)) }),
+          React.createElement(Route, { path: "*", element: React.createElement(ProtectedRoute, null, React.createElement(Home, null)) })
         )
       )
     )
