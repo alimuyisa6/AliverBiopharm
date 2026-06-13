7fbe4ee027308770f963040f8cbd2383ee6c5b5d
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-13 20:51:06 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -935,7 +935,7 @@ export default function Home() {
     cursor: 'pointer' 
   }}
 >
-  <i className="fa-solid fa-book-open-reader"></i> Read More 
+ <i className="fa-solid fa-book-open-reader"></i> TESTING123 
 </button>          
                             </div>
                           </div>
