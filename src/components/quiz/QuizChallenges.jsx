5dd78a52db31824f20b878dbd07ba393cdd5402a
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-14 16:17:27 +0300

    Update QuizChallenges.jsx

--- src/components/quiz/QuizChallenges.jsx
+++ src/components/quiz/QuizChallenges.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+ import { useEffect, useState } from 'react';
 import { getRequest } from '../../api/client';
 
 export default function QuizChallenges({ user }) {
   const [challenge, setChallenge] = useState(null);
   useEffect(() => {
     if (!user) return;
-    getRequest('quiz', 'daily-challenge').then(setChallenge).catch(()=>{});
+    getRequest('interactions', 'daily-challenge').then(setChallenge).catch(() => {});
   }, [user]);
   if (!challenge) return null;
   const percent = (challenge.progress / challenge.target) * 100;
@@ -13,7 +13,7 @@ export default function QuizChallenges({ user }) {
     <div className="daily-challenge-card">
       <i className="fa-solid fa-bolt"></i>
       <div><h4>{challenge.title}</h4><p>{challenge.reward_xp} XP</p></div>
-      <div className="challenge-progress"><div style={{width:`${percent}%`}}></div></div>
+      <div className="challenge-progress"><div style={{ width: `${percent}%` }}></div></div>
       {challenge.completed ? <i className="fa-solid fa-check-circle"></i> : <span>{challenge.progress}/{challenge.target}</span>}
     </div>
   );
