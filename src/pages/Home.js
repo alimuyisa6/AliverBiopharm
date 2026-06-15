f573b852c75a1403685dd89b4accdd5211c9845c
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-15 18:58:39 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import React, { useState, useEffect, useRef } from 'react';
+ import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 import HomeView from './HomeView';
