412ee509e4ec6362a85a957ce458a170c79087c8
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-20 21:28:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './styles/global.css';
 import './styles/component-bundle.css';
 import './styles/quiz.css';
-import './styles/loading.css';
+import './loading/Loading.css';
 import LoadingProvider from './loading/LoadingProvider';
 
 class ErrorBoundary extends React.Component {
