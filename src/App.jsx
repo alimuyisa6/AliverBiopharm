b2ec886fd57a83568a2836ac635383d7724a621d
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-21 01:49:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
  import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -17,11 +17,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/quiz" element={<ProtectedRoute><div>Quiz disabled</div></ProtectedRoute>} />
+          <Route path="/quiz" element={<div>Quiz disabled</div>} />
           <Route path="/past-papers" element={<PastPapers />} />
-          <Route path="/recall" element={<ProtectedRoute><div>Recall disabled</div></ProtectedRoute>} />
-          <Route path="/notes/read" element={<ProtectedRoute><div>Notes disabled</div></ProtectedRoute>} />
-          <Route path="*" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/recall" element={<div>Recall disabled</div>} />
+          <Route path="/notes/read" element={<div>Notes disabled</div>} />
+          <Route path="*" element={<Home />} />
           <Route path="/terms" element={<LegalPage type="terms" />} />
           <Route path="/privacy" element={<LegalPage type="privacy" />} />
         </Routes>
