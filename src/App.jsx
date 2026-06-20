bd16346e752310069bbce53c7bd5f029cd335007
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-21 00:41:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
  import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import PastPapers from './pages/PastPapers';
- import NoteDetail from './pages/NoteDetail';
- import BioRecall from './pages/Recall';
+import NoteDetail from './pages/NoteDetail';
+import BioRecall from './pages/Recall';
 import LegalPage from './pages/LegalPage';
 
 function App() {
@@ -17,14 +17,13 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+          <Route path="/quiz" element={<Quiz />} />
           <Route path="/past-papers" element={<PastPapers />} />
-          <Route path="/recall" element={<ProtectedRoute><BioRecall /></ProtectedRoute>} />
-         <Route path="/notes/read" element={<ProtectedRoute><NoteDetail /></ProtectedRoute>} />
-          <Route path="*" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/terms" element={<LegalPage type="terms" />} />
-<Route path="/privacy" element={<LegalPage type="privacy" />} />
-        
+          <Route path="/recall" element={<BioRecall />} />
+          <Route path="/notes/read" element={<NoteDetail />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/terms" element={<LegalPage type="terms" />} />
+          <Route path="/privacy" element={<LegalPage type="privacy" />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
