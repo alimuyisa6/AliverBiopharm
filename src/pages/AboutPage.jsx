17bd64a09446d8ac4277ae19d89522635f1d2774
alimuyisa6 <alimuyisa6@gmail.com>
2026-06-26 21:37:11 +0300

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -429,7 +429,7 @@ export default function AboutPage() {
           <nav style={{ display: 'flex', gap: '22px' }}>
             <Link to="/privacy" style={{ fontSize: '.875rem', color: 'var(--clr-text-dim)' }}>Privacy Policy</Link>
             <Link to="/terms" style={{ fontSize: '.875rem', color: 'var(--clr-text-dim)' }}>Terms of Use</Link>
-            <Link to="/accessibility" style={{ fontSize: '.875rem', color: 'var(--clr-text-dim)' }}>Accessibility</Link>
+            <Link to="/about" style={{ fontSize: '.875rem', color: 'var(--clr-text-dim)' }}>About Us</Link>
           </nav>
         </div>
       </footer>
