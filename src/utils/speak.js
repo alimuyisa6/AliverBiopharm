c7231f52b0f94f06ea680fdb694771449ed4fe77
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-01 23:09:12 +0300

    Add speakText function for text-to-speech

---
+++ src/utils/speak.js
@@ -0,0 +1,9 @@
+// shared/utils/speak.js
+export function speakText(text) {
+  if ('speechSynthesis' in window) {
+    window.speechSynthesis.cancel();
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 0.9;
+    window.speechSynthesis.speak(utterance);
+  }
+}
