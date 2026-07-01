77d9c26eb32cfc4d20cae9b6479c3c3f9c261117
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-02 00:20:58 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
   import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import HomeView from './HomeView';
+ import HomeView from '../features/home/HomeView';
 import { getSections } from '../api/sections';
 import { useAuth } from '../contexts/AuthContext';
 import {
