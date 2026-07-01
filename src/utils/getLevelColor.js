89e6d76d77c8e8da0556fb5e49dc82fd8102a627
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-01 23:17:18 +0300

    Delete src/utils/getLevelColor.js

--- src/utils/getLevelColor.js
+++
@@ -1,7 +0,0 @@
-// shared/utils/getLevelColor.js
-export function getLevelColor(level) {
-  if (level === 'O-Level') return '#0ab5b5';
-  if (level === 'A-Level') return '#b8873a';
-  if (level === 'Pharmacy') return '#10b981';
-  return 'var(--clr-cyan)';
-}
