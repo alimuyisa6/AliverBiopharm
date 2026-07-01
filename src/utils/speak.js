c6e3de003294bb8e1ea4e3d20350a8b7c67b25a3
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-01 23:17:45 +0300

    Delete src/utils/speak.js

--- src/utils/speak.js
+++
@@ -1,9 +0,0 @@
-// shared/utils/speak.js
-export function speakText(text) {
-  if ('speechSynthesis' in window) {
-    window.speechSynthesis.cancel();
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.rate = 0.9;
-    window.speechSynthesis.speak(utterance);
-  }
-}
