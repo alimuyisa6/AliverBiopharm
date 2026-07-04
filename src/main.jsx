742f029016dabea97400b94a918f2d2a7a10b055
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-04 17:03:56 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
 import './styles/component-bundle.css';
@@ -134,9 +135,11 @@ try {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <ErrorBoundary>
-        <LoadingProvider>
-          <App />
-        </LoadingProvider>
+        <BrowserRouter>
+          <LoadingProvider>
+            <App />
+          </LoadingProvider>
+        </BrowserRouter>
       </ErrorBoundary>
     </React.StrictMode>
   );
