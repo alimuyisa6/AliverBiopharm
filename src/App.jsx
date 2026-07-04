ff458b469449d03dce8e9d48471a48facb232ebe
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-04 17:05:09 +0300

    Refactor routing to use AnimatedRoutes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
  import React, { useEffect, useRef } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import { AuthProvider, ProtectedRoute } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -82,34 +83,42 @@ function ScrollManager() {
   return null;
 }
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <ScrollManager />
+      <Routes location={location} key={location.pathname}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+        <Route path="/past-papers" element={<PastPapers />} />
+        <Route path="/recall" element={<ProtectedRoute><BioRecall /></ProtectedRoute>} />
+        <Route path="/notes/read" element={<ProtectedRoute><NoteDetail /></ProtectedRoute>} />
+        <Route path="/glossary/:slug" element={<ProtectedRoute><Glossary /></ProtectedRoute>} />
+        <Route path="/glossary" element={<ProtectedRoute><Glossary /></ProtectedRoute>} />
+        <Route path="/lab" element={<ProtectedRoute><LearningLab /></ProtectedRoute>} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/flashcards" element={<ProtectedRoute><FlashcardsPage /></ProtectedRoute>} />
+        <Route path="/terms" element={<LegalPage type="terms" />} />
+        <Route path="/privacy" element={<LegalPage type="privacy" />} />
+        <Route path="/classroom" element={<ProtectedRoute><Classroom /></ProtectedRoute>} />
+        <Route path="/classroom/:roomId" element={<ProtectedRoute><ClassroomRoom /></ProtectedRoute>} />
+        <Route path="/tutor/apply" element={<ProtectedRoute><TutorApply /></ProtectedRoute>} />
+        <Route path="/tutor/dashboard" element={<ProtectedRoute><TutorDashboard /></ProtectedRoute>} />
+        <Route path="/info/:slug" element={<InfoPage />} />
+        <Route path="*" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <ScrollManager />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
-          <Route path="/past-papers" element={<PastPapers />} />
-          <Route path="/recall" element={<ProtectedRoute><BioRecall /></ProtectedRoute>} />
-          <Route path="/notes/read" element={<ProtectedRoute><NoteDetail /></ProtectedRoute>} />
-          <Route path="/glossary/:slug" element={<ProtectedRoute><Glossary /></ProtectedRoute>} />
-          <Route path="/glossary" element={<ProtectedRoute><Glossary /></ProtectedRoute>} />
-          <Route path="/lab" element={<ProtectedRoute><LearningLab /></ProtectedRoute>} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/flashcards" element={<ProtectedRoute><FlashcardsPage /></ProtectedRoute>} />
-          <Route path="/terms" element={<LegalPage type="terms" />} />
-          <Route path="/privacy" element={<LegalPage type="privacy" />} />
-          <Route path="/classroom" element={<ProtectedRoute><Classroom /></ProtectedRoute>} />
-          <Route path="/classroom/:roomId" element={<ProtectedRoute><ClassroomRoom /></ProtectedRoute>} />
-          <Route path="/tutor/apply" element={<ProtectedRoute><TutorApply /></ProtectedRoute>} />
-          <Route path="/tutor/dashboard" element={<ProtectedRoute><TutorDashboard /></ProtectedRoute>} />
-          <Route path="/info/:slug" element={<InfoPage />} />
-          <Route path="*" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <AnimatedRoutes />
+    </AuthProvider>
   );
 }
 
