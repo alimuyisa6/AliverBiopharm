8f11c43993eeb55f7a80f6262af831a27bcc5a5f
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-15 12:21:58 +0300

    Add import for profile.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import './styles/Lab.css';
 import './styles/Liveroom.css';
 import './styles/Tutor.css';
 import './styles/Dashboard.css';
+import './styles/profile.css';
 import LoadingProvider from './loading/LoadingProvider';
 
 function showFatalError(title, message, stack) {
