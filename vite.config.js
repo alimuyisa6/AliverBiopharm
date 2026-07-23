a9201c2c1d30f9c2940feec1739ee2c069ef72e4
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-23 18:50:55 +0300

    Fix: Use esbuild minifier instead of terser (terser not installed)

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
