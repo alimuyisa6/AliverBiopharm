78dc07ae21a2631e5acf05718407959d05a6c232
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-23 16:19:25 +0300

    Update ProfilePictureUpload.jsx

--- src/components/ProfilePictureUpload.jsx
+++ src/components/ProfilePictureUpload.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+ import React, { useState, useRef } from 'react';
 import { FaCamera, FaSpinner, FaTrash, FaUser } from 'react-icons/fa6';
 import { uploadProfilePicture, deleteProfilePicture } from '../api/client';
 
