01dace8bd3039505b63a713d69c2ce62c44c6130
alimuyisa6 <alimuyisa6@gmail.com>
2026-07-31 01:10:09 +0300

    Create Skeleton.jsx

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,10 @@
+/* components/Skeleton/Skeleton.jsx */
+export default function Skeleton({ width, height = 16, borderRadius, className = '' }) {
+  return (
+    <div
+      className={`skeleton ${className}`}
+      style={{ width, height, borderRadius }}
+      aria-hidden="true"
+    />
+  );
+}
