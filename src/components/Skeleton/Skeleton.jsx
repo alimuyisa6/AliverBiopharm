3e37808e32780140f1bce1751d84dfc382f64b88
alimuyisa6 <alimuyisa6@gmail.com>
2026-08-16 18:17:43 +0300

    Trim className in Skeleton component

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -2,8 +2,12 @@
 export default function Skeleton({ width, height = 16, borderRadius, className = '' }) {
   return (
     <div
-      className={`skeleton ${className}`}
-      style={{ width, height, borderRadius }}
+      className={`skeleton ${className}`.trim()}
+      style={{
+        width,
+        height,
+        borderRadius
+      }}
       aria-hidden="true"
     />
   );
