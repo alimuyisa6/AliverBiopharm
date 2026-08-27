f147c80ee853a40d542b5160dc3be24d2f9e367b
alimuyisa6 <alimuyisa6@gmail.com>
2026-08-27 06:36:30 +0300

    Add RecallInsightsTab component

---
+++ src/components/recall/RecallInsightsTab.jsx
@@ -0,0 +1,10 @@
+// components/recall/RecallInsightsTab.jsx
+import RecallAnalyticsHub from './RecallAnalyticsHub';
+
+export default function RecallInsightsTab() {
+  return (
+    <div className="recall-tab-content">
+      <RecallAnalyticsHub />
+    </div>
+  );
+}
