edeeedb994289bb150dcb207502c1341ca0966b5
alimuyisa6 <alimuyisa6@gmail.com>
2026-08-31 15:26:30 +0300

    Update HomeView.jsx

--- src/features/home/HomeView.jsx
+++ src/features/home/HomeView.jsx
@@ -1,4 +1,4 @@
-/* features/home/HomeView.jsx */
+ /* features/home/HomeView.jsx */
 import { Link } from 'react-router-dom';
 import Icon from '../../components/Icon/Icon';
 import Button from '../../components/Button/Button';
